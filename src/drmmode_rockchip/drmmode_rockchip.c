b81e5e07533721cc03ce7e325c0b06aa019ddece
Paolo Sabatino <paolo.sabatino@gmail.com>
2020-05-28 14:52:28 +0000

    Changed rockchip to use software cursor for now Added proper bits to create deb package suitable for Ubuntu Focal

--- src/drmmode_rockchip/drmmode_rockchip.c
+++ src/drmmode_rockchip/drmmode_rockchip.c
@@ -119,7 +119,7 @@ struct drmmode_interface rockchip_interface = {
 	CURSORW               /* cursor width */,
 	CURSORH               /* cursor_height */,
 	CURSORPAD             /* cursor padding */,
-	HWCURSOR_API_STANDARD    /* cursor_api */,
+	HWCURSOR_API_NONE    /* cursor_api */,
 	init_plane_for_cursor /* init_plane_for_cursor */,
 	1                     /* vblank_query_supported */,
 	create_custom_gem     /* create_custom_gem */,
